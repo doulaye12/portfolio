d85b075148b80ae62e92b02b18a3ca5e96c61fcc
Abdoulaye Mangane <tmp_mangane74705@orange-sonatel.com>
2025-09-14 15:51:32 +0000

    add author name

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
 import Image from "next/image";
 
-export default function Home() {
+export default function Home({}) {
   return (
     <div>
-      <h1 className="text-2xl">Home page</h1>
+      <div className="flex justify-between">
+        <div className="flex-1">
+          <h1 className="text-2xl">Je suis Abdoulaye</h1>
+        </div>
+        <div className="flex-1">
+          <img src="" alt="" />
+        </div>
+      </div>
     </div>
   );
 }
